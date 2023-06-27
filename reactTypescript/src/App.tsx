e86d45f40d6c1e3c9f52695e9c959ad8b30eab44
erine_n <enatnat.08@gmail.com>
2023-06-27 14:50:31 -0700

    Passing state to children

--- reactTypescript/src/App.tsx
+++ reactTypescript/src/App.tsx
@@ -2,12 +2,17 @@ import Heading from "./components/Heading"
 import Counter from "./components/counter"
 import { Section } from "./components/Section"
 
+import { useState } from "react"
+
 function App() {
+  const [count, setCount] = useState<number>(1)
+
   return  (
     <>
       <Heading title={"Hello"} />
       <Section title="Different Title">This is my section</Section>
-      <Counter/>
+      <Counter setCount={setCount}>Count A is {count} </Counter>
+      <Counter setCount={setCount}>Count B is {count} </Counter>
     </>
    
     )
--- reactTypescript/src/components/Counter.tsx
+++ reactTypescript/src/components/Counter.tsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
+// =================
+// Single state contained in the child
+// =================
+//import { useState } from "react";
 
-const Counter = () => {
-    const [count, setCount] = useState<number>(1)
+
+
+// const Counter = () => {
+//     const [count, setCount] = useState<number>(1)
+//   return (
+//     <>
+//     <h1> Count is {count}</h1>
+//     <button onClick={()=> setCount(prev => prev + 1)}>+</button>
+//     <button onClick={() => setCount(prev => prev - 1)}>-</button>
+
+//     </>
+//   )
+// }
+
+// =================
+// Multiple states - state is passed doewn from parent
+// =================
+import { ReactNode } from "react"
+
+type CounterProps = {
+    setCount: React.Dispatch<React.SetStateAction<number>>,
+    children: ReactNode
+}
+
+const Counter = ({setCount, children}: CounterProps) => {
   return (
     <>
-    <h1> Count is {count}</h1>
+    <h1> Count is {children}</h1>
     <button onClick={()=> setCount(prev => prev + 1)}>+</button>
     <button onClick={() => setCount(prev => prev - 1)}>-</button>
 
     </>
   )
 }
+
 
 export default Counter
